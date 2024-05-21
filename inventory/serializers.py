e901d883500809bb1f77ef466e6bc3251d84607e
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-05-21 23:15:19 +0300

    show review in book detail

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -3,31 +3,36 @@ from rest_framework import serializers
 from .models import Author ,Book ,Review ,Category
 
 
-class AuthorSerializer(serializers.ModelSerializer):
-    
+class AuthorSerializer(serializers.ModelSerializer): 
     class  Meta:
         model = Author 
         fields ='__all__'
-
-class BookListSerializer(serializers.ModelSerializer):
-    
-    class  Meta:
-        model = Book 
-        fields ='__all__'
-class BookDetailSerializer(serializers.ModelSerializer):
-    
-    class  Meta:
-        model = Book 
-        fields ='__all__'
-
-class ReviewSerializer(serializers.ModelSerializer):
-    
-    class  Meta:
-        model = Review 
-        fields ='__all__'
-
-class CategorySerializer(serializers.ModelSerializer):
-    
+class CategorySerializer(serializers.ModelSerializer): 
     class  Meta:
         model = Category 
-        fields ='__all__'
+        fields =['name','image']
+
+class ReviewSerializer(serializers.ModelSerializer):
+     class  Meta:
+        model = Review 
+        fields =['reviewer_name','content','created_at','rating']
+class BookListSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    author =serializers.StringRelatedField()
+
+    class  Meta:
+        model = Book 
+        fields =['title','author','category','image','description','quantity','price']
+
+class BookDetailSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    author =AuthorSerializer()
+    review_book = ReviewSerializer(many=True )
+    class  Meta:
+        model = Book 
+        fields =['title','author','category','image','description','review_book',
+                 'quantity','price','publication_date']
+
+
+
+
--- inventory/urls.py
+++ inventory/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path 
 from .api import BookList ,BookDetail
 urlpatterns = [
+    path('api/list' ,BookList.as_view()),
+    path('api/list/<int:pk>' ,BookDetail.as_view())
 ]
